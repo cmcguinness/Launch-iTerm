956d149d79e773f2ec78df21c5f07da11e489239
Charles McGuinness <github@mcguinness.us>
2016-06-14 16:57:45 -0400

    First release

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Launch-iTerm
-Short JavaScript to launch iTerm at a directory from Finder (uses Apple Open Scripting)
+Short JavaScript to launch iTerm at a directory from Finder
+
+## Overview
+For some reason, my old Launch iTerm at folder script broke.  So I
+wrote a new one, only in JavaScript rather than AppleScript.
+
+You just right click a folder, and find the service.  Boom, you're
+in iTerm at the directory.
+
+## Installation
+1. Launch Automator
+2. Create a new service
+3. Service receives selected folders in Finder
+4. Action is Run JavaScript
+5. Source is in the `iterm.js` file in this repository
+6. Save as whatever you want to see on the services menu
---
+++ iterm.js
@@ -0,0 +1,22 @@
+function run(input, parameters) {
+	// Uncomment this code to trigger a break in the debugger
+	// debugger
+	
+	//	Step one: figure out the folder we were launched from.
+	//	Why is this so painful? I have no idea. Maybe I missed something!
+	var finder = Application('finder');
+	var sel = finder.selection();
+	var url = sel[0].getProperty('url').get();
+	var dir = url.substring(7);
+	//	Because we get a URL, spaces have been replaced by %20s.  Un-do that...
+	dir = dir.replace(/%20/g,' ');
+	
+	//	Now launch iterm, navagate to the right directory
+	var iTerm = Application("iTerm");
+	var win = iTerm.createWindowWithDefaultProfile();
+	var tab = win.currentTab();
+	var ses = tab.currentSession();
+	ses.write({text: 'cd "' + dir + '"'});
+
+	return "";
+}
